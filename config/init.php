ed284c4ee5abe8178a3bac02c3b5d2304e123476
jrlvarghese <jrlvarghese@gmail.com>
2020-05-30 00:41:51 +0530

    Make classes autoload using __autoload(). Add 'config.php' file in config folder.

--- config/init.php
+++ config/init.php
@@ -1,1 +1,7 @@
-init.php
+<?php
+// require_once('lib/Template.php');
+// instead of doing as above we can autoload as follows
+
+function __autoload($class_name){
+    require_once 'lib/'.$class_name.'.php';
+}
