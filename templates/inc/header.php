9b8f2d41e7d445d3990783ad5dfc6618a0b73bb1
jrlvarghese <jrlvarghese@gmail.com>
2020-05-30 01:11:14 +0530

    Add links for the css files.

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -1,1 +1,8 @@
-header.php
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job Lister</title>
+    <link rel='stylesheet' href='https://bootswatch.com/4/flatly/bootstrap.min.css'>
+    <link rel='stylesheet' href='css/styles.css'>
+</head>
