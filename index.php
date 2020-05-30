ef86d3c2c7bbf494446959619fc3ae012e2da7c2
jrlvarghese <jrlvarghese@gmail.com>
2020-05-30 19:14:07 +0530

    Display contents from database.

--- index.php
+++ index.php
@@ -3,4 +3,11 @@ include_once 'config/init.php';
 ?>
 
 <?php
+// instantiate class Job - refer to Job.php
+$job = new Job;
+
 $template = new Template('templates/frontpage.php');
+// using method getAllJobs from Job class
+$template->jobs = $job->getAllJobs();
+
+echo $template;
