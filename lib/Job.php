b12d5577ccb4f351698b8919cefa6b1cfb18c878
jrlvarghese <jrlvarghese@gmail.com>
2020-05-30 17:32:55 +0530

    Create 'Job.php' class.

--- lib/Job.php
+++ lib/Job.php
@@ -1,1 +1,22 @@
-Job.php
+<?php
+// steps:
+// instantiate a Database object -- refer to Database.php
+class Job{
+    private $db;
+    //instantiate Database
+    public function __construct(){
+        $this->db = new Database;
+    }
+    // method to get all jobs listed in db
+    public function getAllJobs(){
+        $this->db->query("SELECT jobs.*, jobcategory.catName AS cname 
+                            FROM jobs
+                            INNER JOIN jobcategory 
+                            ON jobs.categoryId=jobcategory.id 
+                            ORDER BY postDate DESC");
+
+        // assign results
+        $results = $this->db->resultSet();
+        return results;
+    }
+}
