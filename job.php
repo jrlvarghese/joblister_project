3d6eca3d68048c5978a06117fdbb83b26fe33acf
jrlvarghese <jrlvarghese@gmail.com>
2020-06-01 19:30:21 +0530

    Modify 'job.php' file to load data to template.

--- job.php
+++ job.php
@@ -1,1 +1,33 @@
-job.php
+<?php
+include_once 'config/init.php';
+?>
+
+<?php
+// instantiate Job object
+$job = new Job;
+$template = new Template('templates/job-single.php');
+
+$job_id = isset($_GET['id'])?$_GET['id']:null;
+
+$template->job = $job->getJobDetails($job_id);
+
+// // check the category passed
+// $category = isset($_GET['category'])?$_GET['category']:null;
+// if($category){
+//     // if category is set with a variable
+//     $template->jobs = $job->getByCategory($category);
+//     // set the title appended with category
+//     $template->title = $job->getCategoryName($category)->catName;
+
+// }else{
+//     // if there is no query by category
+//     $template->title = 'New Jobs';
+//     // pass Job object contents into template
+//     $template->jobs = $job->getAllJobs();
+// }
+
+// // assign categories
+// $template->categories = $job->getCategories();
+
+echo $template;
+
