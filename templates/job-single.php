03e4d6e3b4f162a3f7e60722493674b44bd46f01
jrlvarghese <jrlvarghese@gmail.com>
2020-06-01 19:44:18 +0530

    Display all fields from job database with proper styling.

--- templates/job-single.php
+++ templates/job-single.php
@@ -4,7 +4,24 @@ include 'inc/header.php';
 ?>
 <main role="main">
 <div class="jumbotron">
-    <h2><?php echo $job->jobTitle; ?></h2>
+    <h2 class="page-header"><?php echo $job->jobTitle.' ('.$job->location.')'; ?></h2>
+    <small>Posted by: <?php echo $job->contactUser.' on '.$job->postDate; ?></small>
+    <hr>
+    <p class="lead"><?php echo $job->description; ?></p>
+    <ul class="list-group">
+        <li class="list-group-item">
+            <strong>Company: <?php echo $job->company; ?></strong>
+        </li>
+        <li class="list-group-item">
+            <strong>Salary: <?php echo $job->salary; ?></strong>
+        </li>
+        <li class="list-group-item">
+            <strong>Contact Email: <?php echo $job->contactEmail; ?></strong>
+        </li>
+    </ul>
+    <br><br>
+    <a href="index.php">Go Back</a>
+    <br><br>
 </div>
 </main>
 <?php
