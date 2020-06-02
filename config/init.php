1baee86a874c55a0cd1a14384f86ce88b32c849e
jrlvarghese <jrlvarghese@gmail.com>
2020-06-02 16:37:31 +0530

    Start session, include helper files inside 'config/init.php'

--- config/init.php
+++ config/init.php
@@ -1,6 +1,10 @@
 <?php
+// start session
+session_start();
 // add configuration file where database details are present
 require_once 'config.php';
+// add helper file
+require_once 'helpers/system_helper.php';
 
 // require_once('lib/Template.php');
 // instead of doing as above we can autoload as follows
