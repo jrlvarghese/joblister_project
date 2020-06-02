d048092b71e1f1f5fec5b10bee5fe557f4585a05
jrlvarghese <jrlvarghese@gmail.com>
2020-06-02 19:47:43 +0530

    Make button to edit and delete in 'job-single.php'.

--- templates/job-single.php
+++ templates/job-single.php
@@ -22,6 +22,14 @@ include 'inc/header.php';
     <br><br>
     <a href="index.php">Go Back</a>
     <br><br>
+
+    <div class="well">
+        <a href="edit.php?id=<?php echo $job->id; ?>" class="btn btn-primary">Edit</a>
+        <form style="display:inline;" method="post" action="job.php">
+            <input type="hidden" name="del_id" value="<?php echo $job->id; ?>">
+            <input type="submit" class="btn btn-danger" value="Delete">
+        </form>
+    </div>
 </div>
 </main>
 <?php
