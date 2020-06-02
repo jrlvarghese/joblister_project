d580aa5a1feed9e1616a58f3103f37b6652adf46
jrlvarghese <jrlvarghese@gmail.com>
2020-06-02 21:32:38 +0530

    Display job details in the edit fields.

--- edit.php
+++ edit.php
@@ -1,1 +1,38 @@
-edit.php
+<?php
+include_once 'config/init.php';
+?>
+
+<?php
+// instantiate Job object
+$job = new Job;
+// check whether the id for job is set
+$job_id = isset($_GET['id'])?$_GET['id']:null;
+
+// check whether the form is submitted or not
+if(isset($_POST['submit'])){
+    $data = array();
+    $data['categoryId'] = $_POST['categoryId'];
+    $data['company'] = $_POST['company'];
+    $data['jobTitle'] = $_POST['jobTitle'];
+    $data['description'] = $_POST['description'];
+    $data['salary'] = $_POST['salary'];
+    $data['location'] = $_POST['location'];
+    $data['contactUser'] = $_POST['contactUser'];
+    $data['contactEmail'] = $_POST['contactEmail'];
+    // insert data and check if that was successful or not
+    // give proper redirect message too
+    if($job->updateJob($job_id,$data)){
+        redirect('index.php','Your job has been updated', 'success');
+    }else{
+        redirect('index.php', 'Something went wrong', 'error');
+    }
+}
+
+$template = new Template('templates/edit-job.php');
+// load values in job field
+$template->job = $job->getJobDetails($job_id);
+// load categories from database
+$template->categories = $job->getCategories();
+
+echo $template;
+
--- templates/edit-job.php
+++ templates/edit-job.php
@@ -1,1 +1,58 @@
-edit-job.php
+<?php
+// include header file
+include 'inc/header.php';
+?>
+<div class="jumbotron">
+<h2 class="page-header">Edit Job Listing</h2>
+<form method="post" action="edit.php?id=<?php echo $job->id; ?>">
+    <div class="form-group">
+        <label>Company</label>
+        <input type="text" class="form-control" name="company" value="<?php echo $job->company; ?>">
+    <div>
+    <!-- Form for inputing data -->
+    <div class="form-group">
+        <label>Category</label>
+        <select class="form-control" name="categoryId">
+        <option value="0">Select Category</option>
+        <!-- load categories from database -->
+        <?php foreach($categories as $category): ?>
+            <?php if($category->id == $job->categoryId): ?>
+                <option value="<?php echo $category->id; ?>" selected><?php echo $category->catName; ?></option>
+            <?php else: ?>
+                <option value="<?php echo $category->id; ?>"><?php echo $category->catName; ?></option>
+            <?php endif; ?>
+        <?php endforeach; ?>
+        </select>
+    </div>
+    
+    <div class="form-group">
+        <label>Job Title</label>
+        <input type="text" class="form-control" name="jobTitle" value="<?php echo $job->jobTitle; ?>">
+    </div>
+    <div class="form-group">
+        <label>Job Description</label>
+        <textarea type="text" class="form-control" name="description" ><?php echo $job->description; ?></textarea>
+    </div>
+    <div class="form-group">    
+        <label>Salary</label>
+        <input type="text" class="form-control" name="salary" value="<?php echo $job->salary; ?>">
+    </div>
+    <div class="form-group">    
+        <label>Location</label>
+        <input type="text" class="form-control" name="location" value="<?php echo $job->location; ?>">
+    </div>
+    <div class="form-group">    
+        <label>Contact User</label>
+        <input type="text" class="form-control" name="contactUser" value="<?php echo $job->contactUser; ?>">
+    </div>
+    <div class="form-group">    
+        <label>Contact Email</label>
+        <input type="text" class="form-control" name="contactEmail" value="<?php echo $job->contactEmail; ?>">
+    </div>
+    <input type="submit" class="btn btn-primary" value="Submit" name="submit"> 
+</form>
+</div>
+<?php
+// include header file
+include 'inc/footer.php';
+?>
