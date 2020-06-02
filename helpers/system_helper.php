42c2cdbbd34e9c32e6baae0067fe4e738d702848
jrlvarghese <jrlvarghese@gmail.com>
2020-06-02 16:34:11 +0530

    Add two methods inside 'system_helper.php'. 'redirect()' and 'displayMessage()'.

--- helpers/system_helper.php
+++ helpers/system_helper.php
@@ -1,1 +1,45 @@
-system_helper.php
+<?php
+function redirect($page=false, $message=null,$message_type=null){
+    if(is_string($page)){
+        $location = $page;
+    }else{
+        $location = $_SERVER['SCRIPT_NAME'];
+    }
+
+    if($message != null){
+        // set message, 
+        // first have to redirect then to display message
+        // that's why message is stored in a session variable
+        $_SESSION['message'] = $message;
+    }
+
+    if($message_type != null){
+        $_SESSION['message_type'] = $message_type;
+    }
+
+    // now redirect
+    header('Location: '.$location);
+    exit;
+}
+
+function displayMessage(){
+    if(!empty($_SESSION['message'])){
+        // if message is set in session 
+        $message = $_SESSION['message'];
+        // check message type
+        if(!empty($_SESSION['message_type'])){
+            $message_type = $_SESSION['message_type'];
+            // check whether its error or success
+            if($message_type == 'error'){
+                echo '<div class="alert alert-danger">'.$message.'</div>';
+            }else{
+                echo '<div class="alert alert-success">'.$message.'</div>';
+            }
+        }
+        // unset message variables from session
+        unset($_SESSION['message']);
+        unset($_SESSION['message_type']);
+    }else{
+        echo '';
+    }
+}
